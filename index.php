899bfdedddaf135849fee33db725bc398c00374a
Jo Cichon <cichon.j@student.greenriver.edu>
2023-05-04 03:28:26 -0400

    Added pages, breakfast, lunch, dinner, orderform1, orderform1, summary, with routes and includes/header

--- index.php
+++ index.php
@@ -18,13 +18,55 @@ $f3->route('GET /', function() {
 }
 );
 
-//Define a default route
+//Define a breakfast route
 $f3->route('GET /breakfast', function() {
 //    echo '<h1>Breakfast Menu</h1>';
     $view = new Template();
     echo $view->render('views/menus/breakfast.html');
 }
 );
+
+//Define a lunch route
+$f3->route('GET /lunch', function() {
+//    echo '<h1>Lunch</h1>';
+    $view = new Template();
+    echo $view->render('views/menus/lunch.html');
+}
+);
+
+//Define a dinner route
+$f3->route('GET /dinner', function() {
+//    echo '<h1>Dinner</h1>';
+    $view = new Template();
+    echo $view->render('views/menus/dinner.html');
+}
+);
+
+//Define a route to orderform1
+$f3->route('GET /orderform1', function() {
+//    echo '<h1>Order Form 1</h1>';
+    $view = new Template();
+    echo $view->render('views/orderforms/orderform1.html');
+}
+);
+
+//Define a route to orderform2
+$f3->route('GET /orderform2', function() {
+//    echo '<h1>Order Form 2</h1>';
+    $view = new Template();
+    echo $view->render('views/orderforms/orderform2.html');
+}
+);
+
+//Define a route to summary
+$f3->route('GET /summary', function() {
+//    echo '<h1>Summary</h1>';
+    $view = new Template();
+    echo $view->render('views/summary.html');
+}
+);
+
+
 
 
 //Run fat free
